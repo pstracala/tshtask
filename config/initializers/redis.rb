5ec5cbdd8d667086b341148dcb06e7e1601fbd15
Paweł Strącała <pawelekstro@gmail.com>
2015-08-12 03:07:39 +0200

    Fix redit

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-end
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
